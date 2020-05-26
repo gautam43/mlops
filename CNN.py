d4ae000f27bbaf64188b21e15af5e29e8183804f
Gautam khatri <gautamkhatri43@gmail.com>
2020-05-26 13:02:43 +0530

    new commit

--- CNN.py
+++ CNN.py
@@ -24,12 +24,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        'cnn_dataset/training_set/',
+        '/dataset/cnn_dataset/training_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        'cnn_dataset/test_set/',
+        '/dataset/cnn_dataset/test_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
