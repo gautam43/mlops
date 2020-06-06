61b2cb88a75d779ee11ed579670be074a30ae68a
Jenkins Automation Server <jenkins@localhost.localdomain>
2020-06-06 16:36:28 +0530

    new update

--- file.py
+++ file.py
@@ -16,6 +16,11 @@ model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_sha
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+
+model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64, 64, 3)))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64, 64, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.summary()
